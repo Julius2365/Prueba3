48f6ef8c2958ff2122d87eff3336a2a36582b339
Julius2365 <julio.hernandez.236541@ciencias.unam.mx>
2023-10-23 01:14:17 -0600

    Primer intento de hacer un CRUD

--- todoapi/todolist/urls.py
+++ todoapi/todolist/urls.py
@@ -1,8 +1,24 @@
 from django.urls import path
+from .views import *
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views import  View
 
-from . import views
+app_name = 'todolist'
+class LibroAPI(View):
+    def post(self, request, *args, **kwargs):
+        # Obtén los datos JSON de la solicitud POST
+        datos = json.loads(request.body)
+
+        # Aquí puedes manejar los datos, por ejemplo, guardarlos en la base de datos
+
+        # Devuelve una respuesta JSON indicando que el libro ha sido creado
+        return JsonResponse({"mensaje": "Libro creado exitosamente!"})
+
 
 urlpatterns = [
-  path('todos/', views.TodoListView.as_view()),
-  path('todos/<int:id>/', views.TodoListView.as_view()),
+    # Otras rutas de tu aplicación...
+    path('api/libros/', csrf_exempt(LibroAPI.as_view()), name='libro-api'),
+    path('v1/post', Post_APIView.as_view()),
+    path('v1/post/<int:pk>/', Post_APIView_Detail.as_view()),
 ]
